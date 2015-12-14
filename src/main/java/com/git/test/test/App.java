bcce0d4b02c399786bc221bb3538a68c75deabb8
Shams <shams.mali@live.com>
2015-12-14 14:53:39 -0800

    Chnages

--- src/main/java/com/git/test/test/App.java
+++ src/main/java/com/git/test/test/App.java
@@ -4,10 +4,12 @@ package com.git.test.test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String f = "sfsdfsdfsdf";
+
+		System.out.println("Hello World!");
+
+		System.out.println("Hello World!" + f);
+	}
 }
