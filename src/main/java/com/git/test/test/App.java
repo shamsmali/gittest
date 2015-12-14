7288e8992935216fe21eedc91d272b76ed3b6374
Shams-1 <shams.mali@live.com>
2015-12-14 15:01:33 -0800

    Chnaged

--- src/main/java/com/git/test/test/App.java
+++ src/main/java/com/git/test/test/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String sfds = "sdf sfdsfsdf sdfsdf";
+    	
+        System.out.println( "Hello World!" + sfds );
     }
 }
