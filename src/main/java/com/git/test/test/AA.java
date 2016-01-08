d9577a65548018cafcf6044e4a9456ca344cdf21
Shams <shams.mali@live.com>
2016-01-08 11:48:53 -0800

    New Files

---
+++ src/main/java/com/git/test/test/AA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git.test.test;
+
+/**
+ * @author shams
+ *
+ */
+public class AA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/git/test/test/A.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.git.test.test;
+
+/**
+ * @author shams
+ *
+ */
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
