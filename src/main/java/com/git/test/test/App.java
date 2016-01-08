c9bf651581f81454886b502fbaf7de39f425f687
Shams <shams.mali@live.com>
2016-01-08 11:45:57 -0800

    Santosh

--- src/main/java/com/git/test/test/App.java
+++ src/main/java/com/git/test/test/App.java
@@ -6,13 +6,6 @@ package com.git.test.test;
  */
 public class App {
 	public static void main(String[] args) {
-		String f = "sfsdfsdfsdf";
-		String sdf = "sdf sdfsdfsdfsdf";
-		System.out.println("Hello World!" + sdf);
-
-		System.out.println("Hello World!" + f);
-        System.out.println("how it looks!" + f);
-        
-		System.out.println("Hello Worsddsfld!" + sdf);
+		System.out.println(" jUST sanotsh");
 	}
 }
